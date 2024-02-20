dd46e61aca053ead682202b65672815b4095471d
Elga J Henry <henryelga2004@gmail.com>
2024-02-20 12:59:46 +0000

    - Changed Main Image - Added Blog Items(temporary)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,9 +7,7 @@
                 <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
                     Do you want to become a developer?
                 </h1>
-                <a 
-                    href="/blog"
-                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
                     Read More
                 </a>
             </div>
@@ -18,32 +16,138 @@
 
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+            <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                width="700" alt="">
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
             <h2 class="text-3xl font-extrabold text-gray-600">
                 Struggling to be a better web developer?
             </h2>
-            
+
             <p class="py-8 text-gray-500 text-s">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
             </p>
 
             <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos
+                molestias porro vero. Vel alias.
             </p>
 
-            <a 
-                href="/blog"
-                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+            <a href="/blog" class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
                 Find Out More
             </a>
         </div>
     </div>
 
+    <div class="sm:grid grid-cols-3 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 1
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 2
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 3
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-3 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 4
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 5
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+
+        <div>
+            <a href="/blog">
+                <img src="https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    width="700" alt="">
+
+                <p class="py-2 text-gray-800 text-xs">Date, Time</p>
+                <p class="font-extrabold text-gray-600 text-s">
+                    Book Name 6
+                </p>
+
+                <p class="py-6 text-gray-500 text-s">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
+                </p>
+
+            </a>
+        </div>
+    </div>
+
     <div class="text-center p-15 bg-black text-white">
-        <h2 class="text-2xl pb-5 text-l"> 
+        <h2 class="text-2xl pb-5 text-l">
             I'm an expert in...
         </h2>
 
@@ -71,7 +175,8 @@
         </h2>
 
         <p class="m-auto w-4/5 text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos
+            temporibus quaerat facere consectetur qui.
         </p>
     </div>
 
@@ -83,11 +188,12 @@
                 </span>
 
                 <h3 class="text-xl font-bold py-10">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa
+                    laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi
+                    ex? Nemo hic placeat et?
                 </h3>
 
-                <a 
-                    href=""
+                <a href=""
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
                     Find Out More
                 </a>
