1367cf14a1744d30c3768cc1880443607e82893b
Elga J Henry <henryelga2004@gmail.com>
2024-03-11 22:03:59 +0000

    Created new pge to buy books: with link to the book in amazon

--- resources/views/buybook.blade.php
+++ resources/views/buybook.blade.php
@@ -22,45 +22,20 @@ $books = $data && isset($data['docs']) ? $data['docs'] : [];
         <div class="sm:grid grid-cols-1 gap-10 w-4/5 mx-auto py-15 border-b border-gray-500">
             @if (count($books) > 0)
                 @foreach ($books as $currentBook)
-                    <div class="p-4 items-center justify-center w-[680px] rounded-xl group sm:flex space-x-6 bg-white shadow-xl bg-opacity-50 hover:rounded-2xl">
-                        <a href="https://openlibrary.org{{ $currentBook['seed'][0] }}" target="_blank">
-                            @if (isset($currentBook['isbn']))
-                                <?php
-                                $isbn = $currentBook['isbn'][0];
-                                $imageUrl = "https://covers.openlibrary.org/b/isbn/{$isbn}-M.jpg";
-                                $imageSize = @getimagesize($imageUrl);
-                                ?>
-                                @if ($imageSize && $imageSize[0] > 10 && $imageSize[1] > 10)
-                                    <div class="flex items-center justify-center">
-                                        <img src="{{ $imageUrl }}" alt="Book Cover" class="rounded-md">
-                                    </div>
-                                @else
-                                    <div class="flex items-center justify-center">
-                                        <img src="https://149348893.v2.pressablecdn.com/wp-content/uploads/2019/03/no-image-available.png"
-                                            alt="Book Cover" class="rounded-md">
-                                    </div>
-                                @endif
-                            @else
-                                <div class="flex items-center justify-center">
-                                    <img src="https://149348893.v2.pressablecdn.com/wp-content/uploads/2019/03/no-image-available.png"
-                                        alt="Book Cover" class="rounded-md">
-                                </div>
-                            @endif
+                    <div
+                        class="p-4 items-center justify-center w-[680px] rounded-xl group sm:flex space-x-6 bg-white shadow-xl bg-opacity-50 hover:rounded-2xl">
 
-                            <br>
+                        <p class="font-extrabold text-gray-600 text-s">{{ $currentBook['title'] }}</p>
 
-                            <p class="font-extrabold text-gray-600 text-s">{{ $currentBook['title'] }}</p>
-
-                            @if (!empty($currentBook['id_amazon'][0]))
-                                <p class="py-2 text-gray-500 text-s">
-                                    <a href="https://www.amazon.com/dp/{{ $currentBook['id_amazon'][0] }}" target="_blank">
-                                        Buy on Amazon
-                                    </a>
-                                </p>
-                            @else
-                                <p class="py-2 text-gray-500 text-s">No Amazon link available</p>
-                            @endif
-                        </a>
+                        @if (!empty($currentBook['id_amazon'][0]))
+                            <p class="py-2 text-gray-500 text-s">
+                                <a href="https://www.amazon.com/dp/{{ $currentBook['id_amazon'][0] }}" target="_blank">
+                                    Buy on Amazon
+                                </a>
+                            </p>
+                        @else
+                            <p class="py-2 text-gray-500 text-s">No Amazon link available</p>
+                        @endif
                     </div>
                 @endforeach
             @else
