9552a18f15d261dfbf1ffc3932edfccbc8cbb51a
Elga J Henry <henryelga2004@gmail.com>
2024-03-13 17:20:04 +0000

    Made changes to blog post page

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -20,81 +20,34 @@
     @if (Auth::check())
         <div class="pt-15 w-4/5 m-auto">
             <a href="/blog/create"
-                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                class="bg-blue-500 uppercase bg-transparent text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl mb-3">
                 Create post
             </a>
         </div>
     @endif
 
     @foreach ($posts as $post)
-        <div class="p-4 pb-15 pt-10 mx-20 my-4 flex items-center w-50 group shadow-xl bg-opacity-50">
-            <img src="{{ asset('images/' . $post->image_path) }}" width="350" class="mr-4" alt="">
-
-            <div>
-                <p class="text-gray-800 text-xs pb-2">By <span class="font-bold italic">{{ $post->user->name }}</span>,
-                    Created
-                    on {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
+        
+        <div class="p-4 pb-15 pt-10 mx-20 my-4 flex items-center w-50 group shadow-2xl bg-opacity-80 rounded-xl" style="position: relative;">
+            <img src="{{ asset('images/' . $post->image_path) }}" width="350" class="mr-4 rounded-xl" alt="">
+            <div class="ml-4 flex-grow">
+                <p class="text-gray-800 text-xs pb-2">By <span class="font-bold italic">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
                 <h2 class="font-extrabold text-gray-600 text-4xl">{{ $post->title }}</h2>
                 <p class="mb-3 py-6 text-gray-700 text-s leading-8 font-light">{{ $post->description }}</p>
-                <a href="/blog/{{ $post->slug }}"
-                    class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">Keep
-                    Reading</a>
+                <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">Keep Reading</a>
             </div>
-
+        
             @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-                <div class="ml-auto">
-                    <a href="/blog/{{ $post->slug }}/edit"
-                        class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">Edit</a>
-                    <form action="/blog/{{ $post->slug }}" method="POST">
-                        @csrf
-                        @method('delete')
-                        <button class="text-red-500 pr-3" type="submit">Delete</button>
-                    </form>
-                </div>
+            <div style="position: absolute; bottom: 10px; right: 10px;">
+                <a href="/blog/{{ $post->slug }}/edit" class="uppercase bg-gray-500 text-gray-100 text-xs font-extrabold py-2 px-4 rounded-xl m-2">Edit</a>
+                <form action="/blog/{{ $post->slug }}" method="POST" style="display: inline;">
+                    @csrf
+                    @method('delete')
+                    <button class="uppercase bg-red-500 text-gray-100 text-xs font-extrabold py-2 px-4 rounded-xl m-2" type="submit">Delete</button>
+                </form>
+            </div>
             @endif
         </div>
-
-        {{-- <div
-            class="p-4 items-center justify-center w-[680px] rounded-xl group sm:flex space-x-6 bg-white  shadow-xl bg-opacity-50 hover:rounded-2xl">
-            <img src="{{ asset('images/' . $post->image_path) }}" width="700" alt="">
-
-            <p class="py-2 text-gray-800 text-xs">By <span
-                    class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on
-                {{ date('jS M Y', strtotime($post->updated_at)) }}</p>
-            <p class="font-extrabold text-gray-600 text-s">
-                {{ $post->title }}
-            </p>
-
-            <p class="py-6 text-gray-700 text-s leading-8 font-light">
-                {{ $post->description }}
-            </p>
-
-
-            <a href="/blog/{{ $post->slug }}"
-                class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                Keep Reading
-            </a>
-
-            @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-                <span class="float-right">
-                    <a href="/blog/{{ $post->slug }}/edit"
-                        class="text-gray-700 italic hover:text-gray-900 pb-1 border-b-2">
-                        Edit
-                    </a>
-                </span>
-
-                <span class="float-right">
-                    <form action="/blog/{{ $post->slug }}" method="POST">
-                        @csrf
-                        @method('delete')
-
-                        <button class="text-red-500 pr-3" type="submit">
-                            Delete
-                        </button>
-
-                    </form>
-                </span>
-            @endif
-        </div> --}}
+        
     @endforeach
 @endsection
